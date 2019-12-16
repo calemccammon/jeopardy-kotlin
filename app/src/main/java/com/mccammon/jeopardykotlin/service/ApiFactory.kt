c888ca5a5c5a702e094caefe1de157bbeb2c1a8d
calemccammon <calemccammon@gmail.com>
2019-12-15 19:19:12 -0500

    logging

--- app/src/main/java/com/mccammon/jeopardykotlin/service/ApiFactory.kt
+++ app/src/main/java/com/mccammon/jeopardykotlin/service/ApiFactory.kt
@@ -2,17 +2,25 @@ package com.mccammon.jeopardykotlin.service
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-object ApiFactory{
-    private val client = OkHttpClient().newBuilder().build()
+object ApiFactory {
+
+    private val logger = HttpLoggingInterceptor()
+    private val client = OkHttpClient().newBuilder()
     val J_SERVICE_API :JServiceApi = retrofit().create(JServiceApi::class.java)
 
-    fun retrofit() : Retrofit = Retrofit.Builder()
-        .client(client)
-        .baseUrl("http://jservice.io/")
-        .addConverterFactory(MoshiConverterFactory.create())
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .build()
+    fun retrofit() : Retrofit  {
+        logger.level = HttpLoggingInterceptor.Level.BODY
+        client.addInterceptor(logger)
+
+        return Retrofit.Builder()
+            .client(client.build())
+            .baseUrl("http://jservice.io/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
 }
--- app/src/main/java/com/mccammon/jeopardykotlin/service/Clue.kt
+++ app/src/main/java/com/mccammon/jeopardykotlin/service/Clue.kt
@@ -2,6 +2,6 @@ package com.mccammon.jeopardykotlin.service
 
 data class Clue(val id: Int, val answer: String, val question: String, val value: Int?,
     val airdate: String, val created_at: String, val updated_at: String, val category_id: Int,
-    val game_id: Int?, val invalid_count: Int?) {
+    val game_id: Int?, val invalid_count: Int?, val category: Category) {
 
 }
